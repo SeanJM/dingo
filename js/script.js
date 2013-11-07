d7913b211aa89972c903b4f5fefd05ceb332308c
Sean J. MacIsaac <seanjmaicsaac@gmail.com>
2013-11-07 14:25:17 -0500

    Updated with change event

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
+/* MIT License */
+// Coded by Sean MacIsaac
+// seanjmacisaac@gmail.com
+
 var dingoMouse = {};
 var dingo = {
   isMobile: function () {
-    return ($(window).width() <= 400);
+    //return ($(window).width() <= 400);
     return (navigator.userAgent.match(/iPhone|iPod|iPad|Android|BlackBerry/)) ? true : false;
   },
   htmlEvents: function () {
     if (dingo.isMobile()) {
-      return ['touchend','touchmove','touchstart','touchleave','keyup','keydown','keypress'];
+      return ['touchend','touchmove','touchstart','touchleave','keyup','keydown','keypress','change'];
     } else {
-      return ['click','mousedown','mouseup','mouseenter','mouseleave','mousemove','keyup','keydown','keypress'];
+      return ['click','mousedown','mouseup','mouseenter','mouseleave','mousemove','keyup','keydown','keypress','change'];
     }
   },
   is: function (k,dingoEvent) {
@@ -21,6 +25,14 @@ var dingo = {
     if (typeof match[3] === 'string') {
       $.each(match[3].split(';'),function (i,k) {
         var _match = k.match(/([a-zA-Z0-9_-]+):([\s\S]*?)$/);
+        _match[2]  = _match[2].replace(/^\s+|$\s+/g,'');
+
+        if (_match[2] === 'true') {
+          _match[2] = true;
+        } else if (_match[2] === 'false') {
+          _match[2] = false;
+        }
+
         options[_match[1]] = _match[2];
       });
     }
@@ -29,12 +41,14 @@ var dingo = {
   },
   swipeEvent: function (options,dingoEvent) {
     var rvalue = false,
+        pageX  = (dingo.isMobile())?options.event.changedTouches[0].pageX:options.event.pageX,
+        pageY  = (dingo.isMobile())?options.event.changedTouches[0].pageY:options.event.pageY,
         lr,
         ud;
     if (options.htmlEvent === 'mousedown') {
       dingoMouse.swipeEvent[dingoEvent] = {
-        x: options.event.pageX,
-        y: options.event.pageY
+        x: pageX,
+        y: pageY
       }
       // A Swipe event only triggers during a certain amount of time
       setTimeout(function () {
@@ -42,14 +56,14 @@ var dingo = {
       },300);
     } else if (options.htmlEvent === 'mouseup') {
       if (dingoMouse.swipeEvent[dingoEvent]) {
-        lr     = dingoMouse.swipeEvent[dingoEvent].x-options.event.pageX;
-        ud     = dingoMouse.swipeEvent[dingoEvent].y-options.event.pageY;
         rvalue = {
-          originX: dingoMouse.swipeEvent[dingoEvent].x,
-          originY: dingoMouse.swipeEvent[dingoEvent].y,
-          options: options,
-          dingo: dingoEvent
+          options : options,
+          dingo   : dingoEvent,
+          originX : dingoMouse.swipeEvent[dingoEvent].x,
+          originY : dingoMouse.swipeEvent[dingoEvent].y
         }
+        lr = dingoMouse.swipeEvent[dingoEvent].x-pageX;
+        ud = dingoMouse.swipeEvent[dingoEvent].y-pageY;
         if (Math.abs(lr) > Math.abs(ud) && Math.abs(lr) > 44) {
           // Left or Right
           if (lr > 0) {
@@ -73,23 +87,24 @@ var dingo = {
   },
   dragEvent: function (options,dingoEvent) {
     var rvalue = false,
-        x,
-        y;
+        pageX  = (dingo.isMobile())?options.event.changedTouches[0].pageX:options.event.pageX,
+        pageY  = (dingo.isMobile())?options.event.changedTouches[0].pageY:options.event.pageY;
+
     if (options.htmlEvent === 'mousedown') {
       dingoMouse.dragEvent[dingoEvent] = {
-        originX: options.event.pageX,
-        originY: options.event.pageY,
+        originX: pageX,
+        originY: pageY,
         dragstart: false
       }
     } else if (options.htmlEvent === 'mousemove' && dingoMouse.dragEvent[dingoEvent]) {
-      if (Math.abs(dingoMouse.dragEvent[dingoEvent].originX-options.event.pageX) > 10 || Math.abs(dingoMouse.dragEvent[dingoEvent].originY-options.event.pageY) > 10) {
+      if (Math.abs(dingoMouse.dragEvent[dingoEvent].originX-pageX) > 10 || Math.abs(dingoMouse.dragEvent[dingoEvent].originY-pageY) > 10) {
         rvalue = {
-          originX: dingoMouse.dragEvent[dingoEvent].x,
-          originY: dingoMouse.dragEvent[dingoEvent].y,
-          pageX: options.event.pageX,
-          pageY: options.event.pageY,
-          options: options,
-          dingo: dingoEvent
+          originX : dingoMouse.dragEvent[dingoEvent].x,
+          originY : dingoMouse.dragEvent[dingoEvent].y,
+          pageX   : pageX,
+          pageY   : pageY,
+          options : options,
+          dingo   : dingoEvent
         }
         if (dingoMouse.dragEvent[dingoEvent].dragstart) {
           rvalue.event = 'drag';
@@ -103,13 +118,13 @@ var dingo = {
     } else if (options.htmlEvent === 'mouseup') {
       if (dingoMouse.dragEvent[dingoEvent].dragstart) {
         rvalue = {
-          originX: dingoMouse.dragEvent[dingoEvent].x,
-          originY: dingoMouse.dragEvent[dingoEvent].y,
-          pageX: x,
-          pageY: y,
-          options: options,
-          dingo: dingoEvent,
-          event: 'dragend'
+          originX : dingoMouse.dragEvent[dingoEvent].x,
+          originY : dingoMouse.dragEvent[dingoEvent].y,
+          pageX   : pageX,
+          pageY   : pageY,
+          options : options,
+          dingo   : dingoEvent,
+          event   : 'dragend'
         }
         dingoMouse.dragEvent[dingoEvent] = false;
       }
@@ -131,6 +146,7 @@ var dingo = {
       dingoEvent = k.dingoEvent;
       swipe      = dingo.swipeEvent(options,dingoEvent);
       drag       = dingo.dragEvent(options,dingoEvent);
+
 
       if (dingo.is(options.htmlEvent,dingoEvent)) {
         dingo[options.htmlEvent][dingoEvent](k.data);
@@ -156,31 +172,3 @@ var dingo = {
     });
   }
 }
-
-dingo.dragend = {
-  'swipe-text': function (options) {
-    console.log('drag-end');
-  }
-}
-
-dingo.dragstart = {
-  'swipe-text': function (options) {
-    console.log('drag-start');
-  }
-}
-
-dingo.drag = {
-  'swipe-text': function (options) {
-    console.log('dragging');
-  }
-}
-
-dingo.swipeleft = {
-  'swipe-text': function (options) {
-    console.log('swipeleft');
-  }
-}
-
-$(function () {
-  dingo.init();
-});
